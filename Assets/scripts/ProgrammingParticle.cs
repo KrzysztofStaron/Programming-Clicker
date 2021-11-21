dbe3ea5a5e74f49eabb126eacfe3b0a1fe90ad33
Krzystof Staroń <kisiel3141@gmail.com>
2021-11-21 08:20:26 +0100

    LanguageKeywords class

--- Assets/scripts/ProgrammingParticle.cs
+++ Assets/scripts/ProgrammingParticle.cs
@@ -13,3 +13,17 @@ public class ProgrammingParticle : MonoBehaviour
       GameObject particle = Instantiate(textParticle, pos, Quaternion.identity);
     }
 }
+
+
+[System.Serializable]
+public class LanguageKeywords
+{
+    public string link;
+    public int keywords;
+
+    public static LanguageKeywords load(string patch)
+    {
+      TextAsset targetFile = Resources.Load<TextAsset>(path);
+      return JsonUtility.FromJson<LanguageKeywords>(targetFile.text);
+    }
+}
