861332287f31ac604114d3f23f2ef36d0c63789c
Krzystof Staroń <kisiel3141@gmail.com>
2021-11-21 13:00:07 +0100

    yes

--- Assets/scripts/ProgrammingParticle.cs
+++ Assets/scripts/ProgrammingParticle.cs
@@ -1,20 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ProgrammingParticle : MonoBehaviour
 {
   [SerializeField] GameObject textParticle;
   [SerializeField] Camera cam;
+  [SerializeField] TextAsset targetFile
     void OnMouseDown()
     {
       Vector3 pos = cam.ScreenToWorldPoint(Input.mousePosition);
       pos.z = 0;
       GameObject particle = Instantiate(textParticle, pos, Quaternion.identity);
+      Debug.Log(LanguageKeywords.load("json/html.json").keywords[4]);
     }
 }
 
-
+// 6 * 5 pompek
 [System.Serializable]
 public class LanguageKeywords
 {
@@ -23,7 +25,7 @@ public class LanguageKeywords
 
     public static LanguageKeywords load(string patch)
     {
-      TextAsset targetFile = Resources.Load<TextAsset>(path);
+      TextAsset targetFile = Resources.Load<TextAsset>(patch);
       return JsonUtility.FromJson<LanguageKeywords>(targetFile.text);
     }
 }
